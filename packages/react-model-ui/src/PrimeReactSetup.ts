5765c3db1bd99bbeaf9b45dfc8898ef73fc4c6dd
Hardiksinh Gohil <hardiksinh.gohil@secura-tech.com>
2025-08-01 20:42:41 +0530

    chore: CSS filepath fixes

--- packages/react-model-ui/src/PrimeReactSetup.ts
+++ packages/react-model-ui/src/PrimeReactSetup.ts
@@ -5,5 +5,4 @@
 import 'primeicons/primeicons.css';
 import 'primereact/resources/primereact.min.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
-import './style/primereact-overrides.css';
-import './style/table.css';
+import '../style/primereact-overrides.css';
