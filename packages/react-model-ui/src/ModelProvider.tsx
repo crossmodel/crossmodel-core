4825d36adf1327a3c22e2a361982c03238363a73
Hardiksinh Gohil <hardiksinh.gohil@secura-tech.com>
2025-08-01 12:06:52 +0530

    Garbage code removal

--- packages/react-model-ui/src/ModelProvider.tsx
+++ packages/react-model-ui/src/ModelProvider.tsx
@@ -4,8 +4,6 @@
 
 import { CrossModelRoot } from '@crossmodel/protocol';
 import { URI } from '@theia/core';
-import 'primeicons/primeicons.css';
-import 'primereact/resources/primereact.min.css';
 import * as React from 'react';
 import { useImmerReducer } from 'use-immer';
 import {
