32cc30ab2eb15154a409acf1d3e0f52e451bd298
Harmen Wessels <97173058+harmen-xb@users.noreply.github.com>
2025-08-02 10:01:14 +0200

    feat:  Getting started, updated docs & publish installers on release (#160)
    * First test for Windows electron packaging
    
    * Added missing checkout
    
    * Removed condition on building dev package
    
    * Updated package commands for extensions.
    
    * Update electron dependencies and config
    
    * Updated icon to 256 pixels
    
    * Removed package post for electron
    
    * Removed package post call from package scripts
    
    * Updated dist path for upload action
    
    * Updated electron build and package logic to extract crossmodel extensions during package process.
    
    * Added matrix for packaging for all OSes
    
    * Fixed electron build & update workflow
    
    * Added github token for packaging MacOS.
    
    * Disabled publishing for macos.
    
    * Changed installer artifact name to runner os, i.s.o. matrix os.
    Updated libavoid.wasm copy command.
    
    * Updated workflow to also build for MacOS arm64 and separated publishing of artifacts per installation type.
    
    * Made splash screen a bit transparent.
    Reverted back to ubuntu 22.
    Added license file for installers.
    
    * Updated README, added screenshots and moved developer info to CONTRIBUTING.
    
    * Added github token to solve rate limit issue
    
    * Moved doc images into own folder.
    Added crossmodel logo
    Updated electron build file and product names, updated workflow accordingly.
    Updated README.
    
    * Created CrossModel Getting Started widget
    
    * Updated getting started widget.
    
    * Disabled compression on binary artifacts.
    
    * Moved packaging logic to release workflow.
    Revert feature workflow.
    
    * Fixed package script error in extensions.
    Small changes in workflows

--- packages/product/src/browser/product-frontend-module.ts
+++ packages/product/src/browser/product-frontend-module.ts
@@ -1,10 +1,12 @@
 /********************************************************************************
  * Copyright (c) 2024 CrossBreeze.
  ********************************************************************************/
-import { ApplicationShell, ApplicationShellOptions, FrontendApplication } from '@theia/core/lib/browser';
+import { ApplicationShell, ApplicationShellOptions, FrontendApplication, WidgetFactory } from '@theia/core/lib/browser';
 import { ContainerModule } from '@theia/core/shared/inversify';
+import { GettingStartedWidget } from '@theia/getting-started/lib/browser/getting-started-widget';
 import { ThemeServiceWithDB } from '@theia/monaco/lib/browser/monaco-indexed-db';
 import { CrossModelFrontendApplication } from './cross-model-frontend-application';
+import { CrossModelGettingStartedWidget } from './cross-model-getting-started-widget';
 import { CrossModelThemeService } from './cross-model-theme-service';
 
 export default new ContainerModule((bind, _unbind, _isBound, rebind) => {
@@ -17,4 +19,13 @@ export default new ContainerModule((bind, _unbind, _isBound, rebind) => {
          initialSizeRatio: 0.25 // default: 0.382
       }
    });
+
+   // Override the getting started widget
+   bind(CrossModelGettingStartedWidget).toSelf();
+   bind(WidgetFactory)
+      .toDynamicValue(context => ({
+         id: GettingStartedWidget.ID,
+         createWidget: () => context.container.get<CrossModelGettingStartedWidget>(CrossModelGettingStartedWidget)
+      }))
+      .inSingletonScope();
 });
