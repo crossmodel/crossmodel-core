09009914f79e8c138080057d3f7ad365eaeeaee9
Harmen Wessels <harmen@x-breeze.com>
2025-08-09 10:23:37 +0000

    Fixed errors in test paths

--- packages/server/test/language-server/serializer/cross-model-serializer.test.js
+++ packages/server/test/language-server/serializer/cross-model-serializer.test.js
@@ -4,7 +4,14 @@
 import { beforeAll, describe, expect, test } from '@jest/globals';
 import { URI } from 'langium';
 import _ from 'lodash';
-import { createEntityNode, createLogicalAttribute, createLogicalEntity, createRelationship, createRelationshipEdge, createSystemDiagram } from '../../src/language-server/util/ast-util.js';
+import {
+   createEntityNode,
+   createLogicalAttribute,
+   createLogicalEntity,
+   createRelationship,
+   createRelationshipEdge,
+   createSystemDiagram
+} from '../../../src/language-server/util/ast-util.js';
 import { customer } from '../test-utils/test-documents/entity/customer.js';
 import { sub_customer } from '../test-utils/test-documents/entity/sub_customer.js';
 import { sub_customer_cycle } from '../test-utils/test-documents/entity/sub_customer_cycle.js';
@@ -12,136 +19,138 @@ import { sub_customer_multi } from '../test-utils/test-documents/entity/sub_cust
 import { createCrossModelTestServices, parseDocuments, parseLogicalEntity, testUri } from '../test-utils/utils.js';
 const services = createCrossModelTestServices();
 describe('CrossModelLexer', () => {
-    let serializer;
-    beforeAll(() => {
-        serializer = services.serializer.Serializer;
-    });
-    describe('Serialize entity', () => {
-        let crossModelRoot;
-        let crossModelRootWithoutAttributes;
-        let crossModelRootWithAttributesDifPlace;
-        beforeAll(() => {
-            crossModelRoot = { $type: 'CrossModelRoot' };
-            crossModelRoot.entity = createLogicalEntity(crossModelRoot, 'testId', 'test Name', {
-                description: 'Test description'
-            });
-            crossModelRootWithoutAttributes = _.cloneDeep(crossModelRoot);
-            crossModelRoot.entity.attributes = [
-                createLogicalAttribute(crossModelRoot.entity, 'Attribute1', 'Attribute 1'),
-                createLogicalAttribute(crossModelRoot.entity, 'Attribute2', 'Attribute 2')
-            ];
-            crossModelRootWithAttributesDifPlace = { $type: 'CrossModelRoot' };
-            crossModelRootWithAttributesDifPlace.entity = createLogicalEntity(crossModelRoot, 'testId', 'test Name', {
-                description: 'Test description'
-            });
-            crossModelRootWithAttributesDifPlace.entity.attributes = [
-                createLogicalAttribute(crossModelRoot.entity, 'Attribute1', 'Attribute 1'),
-                createLogicalAttribute(crossModelRoot.entity, 'Attribute2', 'Attribute 2')
-            ];
-        });
-        test('serialize entity with attributes', () => {
-            const parseResult = serializer.serialize(crossModelRoot);
-            expect(parseResult).toBe(expected_result);
-        });
-        test('serialize entity without attributes', () => {
-            const parseResult = serializer.serialize(crossModelRootWithoutAttributes);
-            expect(parseResult).toBe(expected_result2);
-        });
-        test('serialize entity with attributes in different place', () => {
-            const parseResult = serializer.serialize(crossModelRootWithAttributesDifPlace);
-            expect(parseResult).toBe(expected_result3);
-        });
-    });
-    describe('Serialize relationship', () => {
-        let crossModelRoot;
-        beforeAll(() => {
-            crossModelRoot = {
-                $type: 'CrossModelRoot'
-            };
-            const ref1 = {
-                $refText: 'Ref1',
-                ref: createLogicalEntity(crossModelRoot, 'Ref1', 'test Name', {
-                    description: 'Test description'
-                })
-            };
-            const ref2 = {
-                $refText: 'Ref2',
-                ref: createLogicalEntity(crossModelRoot, 'Ref2', 'test Name', {
-                    description: 'Test description'
-                })
-            };
-            crossModelRoot.relationship = createRelationship(crossModelRoot, 'testId', 'test Name', ref1, ref2, {
-                description: 'Test description'
-            });
-        });
-        test('serialize entity with attributes', () => {
-            const parseResult = serializer.serialize(crossModelRoot);
-            expect(parseResult).toBe(expected_result4);
-        });
-    });
-    describe('Serialize diagram', () => {
-        let crossModelRoot;
-        beforeAll(() => {
-            crossModelRoot = {
-                $type: 'CrossModelRoot'
-            };
-            const ref1 = {
-                $refText: 'Ref1',
-                ref: createLogicalEntity(crossModelRoot, 'Ref1', 'test Name', {
-                    description: 'Test description'
-                })
-            };
-            const ref2 = {
-                $refText: 'Ref2',
-                ref: createLogicalEntity(crossModelRoot, 'Ref2', 'test Name', {
-                    description: 'Test description'
-                })
-            };
-            const ref3 = {
-                $refText: 'Ref3',
-                ref: createRelationship(crossModelRoot, 'testId', 'test Name', ref1, ref2, {
-                    description: 'Test description'
-                })
-            };
-            crossModelRoot.systemDiagram = createSystemDiagram(crossModelRoot, 'testId');
-            crossModelRoot.systemDiagram.nodes = [
-                createEntityNode(crossModelRoot.systemDiagram, 'Node1', ref1, { x: 100, y: 101 }, { width: 102, height: 102 }),
-                createEntityNode(crossModelRoot.systemDiagram, 'Node2', ref2, { x: 100, y: 101 }, { width: 102, height: 102 })
-            ];
-            crossModelRoot.systemDiagram.edges = [
-                createRelationshipEdge(crossModelRoot.systemDiagram, 'Edge1', ref3, { $refText: 'A' }, { $refText: 'B' })
-            ];
-        });
-        test('serialize entity with attributes', () => {
-            const parseResult = serializer.serialize(crossModelRoot);
-            expect(parseResult).toBe(expected_result5);
-        });
-    });
-    describe('Serialize entity with inheritance', () => {
-        const customerDocumentUri = testUri('customer');
-        beforeAll(async () => {
-            await parseDocuments({ services, text: customer, documentUri: customerDocumentUri });
-        });
-        test('Single inheritance', async () => {
-            const subCustomer = await parseLogicalEntity({ services, text: sub_customer });
-            expect(subCustomer.superEntities).toHaveLength(1);
-            expect(subCustomer.superEntities[0].$refText).toBe('Customer');
-        });
-        test('Multiple inheritance', async () => {
-            const subCustomer = await parseLogicalEntity({ services, text: sub_customer_multi });
-            expect(subCustomer.superEntities).toHaveLength(2);
-            expect(subCustomer.superEntities[0].$refText).toBe('Customer');
-            expect(subCustomer.superEntities[1].$refText).toBe('SubCustomer');
-        });
-        test('Inheritance Cycle', async () => {
-            services.shared.workspace.LangiumDocuments.deleteDocument(URI.parse(customerDocumentUri));
-            const newCustomer = await parseLogicalEntity({ services, text: sub_customer_cycle, documentUri: 'customer', validation: true });
-            expect(newCustomer.$document.diagnostics).toBeDefined();
-            expect(newCustomer.$document.diagnostics).toEqual(expect.arrayContaining([
-                expect.objectContaining({ message: 'Inheritance cycle detected: Customer -> SubCustomer -> Customer.' })
-            ]));
-        });
-    });
+   let serializer;
+   beforeAll(() => {
+      serializer = services.serializer.Serializer;
+   });
+   describe('Serialize entity', () => {
+      let crossModelRoot;
+      let crossModelRootWithoutAttributes;
+      let crossModelRootWithAttributesDifPlace;
+      beforeAll(() => {
+         crossModelRoot = { $type: 'CrossModelRoot' };
+         crossModelRoot.entity = createLogicalEntity(crossModelRoot, 'testId', 'test Name', {
+            description: 'Test description'
+         });
+         crossModelRootWithoutAttributes = _.cloneDeep(crossModelRoot);
+         crossModelRoot.entity.attributes = [
+            createLogicalAttribute(crossModelRoot.entity, 'Attribute1', 'Attribute 1'),
+            createLogicalAttribute(crossModelRoot.entity, 'Attribute2', 'Attribute 2')
+         ];
+         crossModelRootWithAttributesDifPlace = { $type: 'CrossModelRoot' };
+         crossModelRootWithAttributesDifPlace.entity = createLogicalEntity(crossModelRoot, 'testId', 'test Name', {
+            description: 'Test description'
+         });
+         crossModelRootWithAttributesDifPlace.entity.attributes = [
+            createLogicalAttribute(crossModelRoot.entity, 'Attribute1', 'Attribute 1'),
+            createLogicalAttribute(crossModelRoot.entity, 'Attribute2', 'Attribute 2')
+         ];
+      });
+      test('serialize entity with attributes', () => {
+         const parseResult = serializer.serialize(crossModelRoot);
+         expect(parseResult).toBe(expected_result);
+      });
+      test('serialize entity without attributes', () => {
+         const parseResult = serializer.serialize(crossModelRootWithoutAttributes);
+         expect(parseResult).toBe(expected_result2);
+      });
+      test('serialize entity with attributes in different place', () => {
+         const parseResult = serializer.serialize(crossModelRootWithAttributesDifPlace);
+         expect(parseResult).toBe(expected_result3);
+      });
+   });
+   describe('Serialize relationship', () => {
+      let crossModelRoot;
+      beforeAll(() => {
+         crossModelRoot = {
+            $type: 'CrossModelRoot'
+         };
+         const ref1 = {
+            $refText: 'Ref1',
+            ref: createLogicalEntity(crossModelRoot, 'Ref1', 'test Name', {
+               description: 'Test description'
+            })
+         };
+         const ref2 = {
+            $refText: 'Ref2',
+            ref: createLogicalEntity(crossModelRoot, 'Ref2', 'test Name', {
+               description: 'Test description'
+            })
+         };
+         crossModelRoot.relationship = createRelationship(crossModelRoot, 'testId', 'test Name', ref1, ref2, {
+            description: 'Test description'
+         });
+      });
+      test('serialize entity with attributes', () => {
+         const parseResult = serializer.serialize(crossModelRoot);
+         expect(parseResult).toBe(expected_result4);
+      });
+   });
+   describe('Serialize diagram', () => {
+      let crossModelRoot;
+      beforeAll(() => {
+         crossModelRoot = {
+            $type: 'CrossModelRoot'
+         };
+         const ref1 = {
+            $refText: 'Ref1',
+            ref: createLogicalEntity(crossModelRoot, 'Ref1', 'test Name', {
+               description: 'Test description'
+            })
+         };
+         const ref2 = {
+            $refText: 'Ref2',
+            ref: createLogicalEntity(crossModelRoot, 'Ref2', 'test Name', {
+               description: 'Test description'
+            })
+         };
+         const ref3 = {
+            $refText: 'Ref3',
+            ref: createRelationship(crossModelRoot, 'testId', 'test Name', ref1, ref2, {
+               description: 'Test description'
+            })
+         };
+         crossModelRoot.systemDiagram = createSystemDiagram(crossModelRoot, 'testId');
+         crossModelRoot.systemDiagram.nodes = [
+            createEntityNode(crossModelRoot.systemDiagram, 'Node1', ref1, { x: 100, y: 101 }, { width: 102, height: 102 }),
+            createEntityNode(crossModelRoot.systemDiagram, 'Node2', ref2, { x: 100, y: 101 }, { width: 102, height: 102 })
+         ];
+         crossModelRoot.systemDiagram.edges = [
+            createRelationshipEdge(crossModelRoot.systemDiagram, 'Edge1', ref3, { $refText: 'A' }, { $refText: 'B' })
+         ];
+      });
+      test('serialize entity with attributes', () => {
+         const parseResult = serializer.serialize(crossModelRoot);
+         expect(parseResult).toBe(expected_result5);
+      });
+   });
+   describe('Serialize entity with inheritance', () => {
+      const customerDocumentUri = testUri('customer');
+      beforeAll(async () => {
+         await parseDocuments({ services, text: customer, documentUri: customerDocumentUri });
+      });
+      test('Single inheritance', async () => {
+         const subCustomer = await parseLogicalEntity({ services, text: sub_customer });
+         expect(subCustomer.superEntities).toHaveLength(1);
+         expect(subCustomer.superEntities[0].$refText).toBe('Customer');
+      });
+      test('Multiple inheritance', async () => {
+         const subCustomer = await parseLogicalEntity({ services, text: sub_customer_multi });
+         expect(subCustomer.superEntities).toHaveLength(2);
+         expect(subCustomer.superEntities[0].$refText).toBe('Customer');
+         expect(subCustomer.superEntities[1].$refText).toBe('SubCustomer');
+      });
+      test('Inheritance Cycle', async () => {
+         services.shared.workspace.LangiumDocuments.deleteDocument(URI.parse(customerDocumentUri));
+         const newCustomer = await parseLogicalEntity({ services, text: sub_customer_cycle, documentUri: 'customer', validation: true });
+         expect(newCustomer.$document.diagnostics).toBeDefined();
+         expect(newCustomer.$document.diagnostics).toEqual(
+            expect.arrayContaining([
+               expect.objectContaining({ message: 'Inheritance cycle detected: Customer -> SubCustomer -> Customer.' })
+            ])
+         );
+      });
+   });
 });
 const expected_result = `entity:
     id: testId
--- packages/server/test/language-server/test-utils/utils.js
+++ packages/server/test/language-server/test-utils/utils.js
@@ -4,67 +4,67 @@
 import { EmptyFileSystem, URI } from 'langium';
 import { parseDocument as langiumParseDocument } from 'langium/test';
 import path from 'path';
-import { createCrossModelServices } from '../../src/language-server/cross-model-module.js';
-import { isDataModel, isLogicalEntity, isMapping, isRelationship, isSystemDiagram } from '../../src/language-server/generated/ast.js';
-import { findSemanticRoot } from '../../src/language-server/util/ast-util.js';
+import { createCrossModelServices } from '../../../src/language-server/cross-model-module.js';
+import { isDataModel, isLogicalEntity, isMapping, isRelationship, isSystemDiagram } from '../../../src/language-server/generated/ast.js';
+import { findSemanticRoot } from '../../../src/language-server/util/ast-util.js';
 export function createCrossModelTestServices(context = EmptyFileSystem) {
-    return createCrossModelServices(context).CrossModel;
+   return createCrossModelServices(context).CrossModel;
 }
 export async function parseDocument(input) {
-    if (input.documentUri) {
-        const fileSystemProvider = input.services.shared.workspace.FileSystemProvider;
-        if (fileSystemProvider instanceof MockFileSystemProvider) {
-            fileSystemProvider.setFile(URI.parse(input.documentUri), input.text);
-        }
-    }
-    return langiumParseDocument(input.services, input.text, input);
+   if (input.documentUri) {
+      const fileSystemProvider = input.services.shared.workspace.FileSystemProvider;
+      if (fileSystemProvider instanceof MockFileSystemProvider) {
+         fileSystemProvider.setFile(URI.parse(input.documentUri), input.text);
+      }
+   }
+   return langiumParseDocument(input.services, input.text, input);
 }
 export async function parseDocuments(...inputs) {
-    return Promise.all(inputs.map(parseDocument));
+   return Promise.all(inputs.map(parseDocument));
 }
 export async function parseSemanticRoot(input, assert, guard) {
-    const document = await parseDocument(input);
-    expect(document.parseResult.lexerErrors).toHaveLength(assert.lexerErrors ?? 0);
-    expect(document.parseResult.parserErrors).toHaveLength(assert.parserErrors ?? 0);
-    const semanticRoot = findSemanticRoot(document, guard);
-    expect(semanticRoot).toBeDefined();
-    semanticRoot.$document = document;
-    return semanticRoot;
+   const document = await parseDocument(input);
+   expect(document.parseResult.lexerErrors).toHaveLength(assert.lexerErrors ?? 0);
+   expect(document.parseResult.parserErrors).toHaveLength(assert.parserErrors ?? 0);
+   const semanticRoot = findSemanticRoot(document, guard);
+   expect(semanticRoot).toBeDefined();
+   semanticRoot.$document = document;
+   return semanticRoot;
 }
 export async function parseLogicalEntity(input, assert = {}) {
-    return parseSemanticRoot(input, assert, isLogicalEntity);
+   return parseSemanticRoot(input, assert, isLogicalEntity);
 }
 export async function parseRelationship(input, assert = {}) {
-    return parseSemanticRoot(input, assert, isRelationship);
+   return parseSemanticRoot(input, assert, isRelationship);
 }
 export async function parseSystemDiagram(input, assert = {}) {
-    return parseSemanticRoot(input, assert, isSystemDiagram);
+   return parseSemanticRoot(input, assert, isSystemDiagram);
 }
 export async function parseMapping(input, assert = {}) {
-    return parseSemanticRoot(input, assert, isMapping);
+   return parseSemanticRoot(input, assert, isMapping);
 }
 export async function parseDataModel(input, assert = {}) {
-    return parseSemanticRoot(input, assert, isDataModel);
+   return parseSemanticRoot(input, assert, isDataModel);
 }
 export const MockFileSystem = {
-    fileSystemProvider: () => new MockFileSystemProvider()
+   fileSystemProvider: () => new MockFileSystemProvider()
 };
 export class MockFileSystemProvider {
-    constructor() {
-        this.fileContent = new Map();
-    }
-    setFile(uri, content) {
-        this.fileContent.set(uri.toString(), content);
-    }
-    async readFile(uri) {
-        return this.fileContent.get(uri.toString()) ?? '';
-    }
-    async readDirectory(uri) {
-        return [];
-    }
+   constructor() {
+      this.fileContent = new Map();
+   }
+   setFile(uri, content) {
+      this.fileContent.set(uri.toString(), content);
+   }
+   async readFile(uri) {
+      return this.fileContent.get(uri.toString()) ?? '';
+   }
+   async readDirectory(uri) {
+      return [];
+   }
 }
 export function testUri(...segments) {
-    // making sure the URI works on both Windows and Unix
-    return 'test:///' + path.posix.join(...segments);
+   // making sure the URI works on both Windows and Unix
+   return 'test:///' + path.posix.join(...segments);
 }
 //# sourceMappingURL=utils.js.map
