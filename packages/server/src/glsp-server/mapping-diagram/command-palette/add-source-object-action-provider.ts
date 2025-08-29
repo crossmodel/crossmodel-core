ef9b5adc1af0c7dd6467954b7e8977d68b1f20b3
Harmen Wessels <harmen@x-breeze.com>
2025-08-29 11:20:59 +0000

    Refactored add-source-object-action-provider to re-use modelState of superclass.

--- packages/server/src/glsp-server/mapping-diagram/command-palette/add-source-object-action-provider.ts
+++ packages/server/src/glsp-server/mapping-diagram/command-palette/add-source-object-action-provider.ts
@@ -3,17 +3,17 @@
  ********************************************************************************/
 import { AddSourceObjectOperation, codiconCSSString } from '@crossmodel/protocol';
 import { Args, CommandPaletteActionProvider, GModelElement, LabeledAction, Point } from '@eclipse-glsp/server';
-import { inject, injectable } from 'inversify';
+import { injectable } from 'inversify';
 import { SourceObject } from '../../../language-server/generated/ast.js';
 import { MappingModelState } from '../model/mapping-model-state.js';
 
 @injectable()
 export class MappingDiagramCommandPaletteActionProvider extends CommandPaletteActionProvider {
-   @inject(MappingModelState) protected state!: MappingModelState;
+   declare protected modelState: MappingModelState;
 
    getPaletteActions(_selectedElementIds: string[], _selectedElements: GModelElement[], position: Point, args?: Args): LabeledAction[] {
-      const completionItems = this.state.services.language.references.ScopeProvider.complete({
-         container: { globalId: this.state.mapping.id! },
+      const completionItems = this.modelState.services.language.references.ScopeProvider.complete({
+         container: { globalId: this.modelState.mapping.id! },
          syntheticElements: [{ property: 'sources', type: SourceObject }],
          property: 'entity'
       });
