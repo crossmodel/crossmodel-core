00cd0f91a2be46e997c437276f4cf9cab57ff340
Harmen Wessels <harmen@x-breeze.com>
2025-12-10 17:11:04 +0000

    Refactored shortcut icon rendering to one common entity diagram node class.

--- packages/glsp-client/src/browser/system-diagram/views.tsx
+++ packages/glsp-client/src/browser/system-diagram/views.tsx
@@ -4,64 +4,15 @@
 /** @jsx svg */
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable react/jsx-key */
-import {
-   GGraph,
-   GGraphView,
-   GNode,
-   hasArgs,
-   Hoverable,
-   RenderingContext,
-   Selectable,
-   TYPES,
-   ViewerOptions,
-   svg
-} from '@eclipse-glsp/client';
+import { GGraph, GGraphView, RenderingContext, TYPES, ViewerOptions, svg } from '@eclipse-glsp/client';
 import { inject } from '@theia/core/shared/inversify';
 import { injectable } from 'inversify';
 import { ReactNode } from 'react';
 import { VNode, VNodeStyle } from 'snabbdom';
-import { CrossModelEdgeView, DiagramNodeView } from '../views';
+import { CrossModelEdgeView, EntityDiagramNodeView } from '../views';
 
 @injectable()
-export class EntityNodeView extends DiagramNodeView {
-   override render(node: Readonly<GNode & Hoverable & Selectable>, context: RenderingContext): VNode | undefined {
-      const view = super.render(node, context);
-
-      // Add external entity icon if isExternal flag is true
-      const nodeHasArgs = hasArgs(node);
-      const isExternal = nodeHasArgs && node.args.isExternal === true;
-
-      if (view && isExternal) {
-         this.addExternalEntityIcon(view, node);
-      }
-
-      return view;
-   }
-
-   protected addExternalEntityIcon(view: VNode, node: Readonly<GNode>): void {
-      // Calculate icon position (bottom-left corner)
-      const iconX = 5;
-      const iconY = node.bounds.height - 20;
-
-      // Create the external entity icon (Windows-style shortcut arrow)
-      const icon: any = (
-         <g class-external-entity-icon={true} transform={`translate(${iconX}, ${iconY})`}>
-            {/* White background box for visibility */}
-            <rect x='0' y='5' width='10' height='10' fill='white' stroke='black' stroke-width='0.5' rx='1' />
-
-            {/* Arrow pointing up-right */}
-            <path d='M 2 13 L 2 7 L 8 7' fill='none' stroke='black' stroke-width='1' />
-            <polyline points='6,5 8,7 10,5' fill='none' stroke='black' stroke-width='1' />
-         </g>
-      );
-
-      // Add icon to the view's children
-      if (!view.children) {
-         view.children = [];
-      }
-      view.children.push(icon);
-   }
-}
+export class EntityNodeView extends EntityDiagramNodeView {}
 
 @injectable()
 export class RelationshipEdgeView extends CrossModelEdgeView {}
